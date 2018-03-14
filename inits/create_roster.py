99701dc76a39d48af240141c0a59756ff88c11b5
Bald Mike <baldmike312@gmail.com>
2018-03-14 16:21:57 -0500

    roster script

---
+++ inits/create_roster.py
@@ -0,0 +1,26 @@
+# This will create a user
+# run from resQmia ROOT
+# python manage.py shell
+# execfile('./inits/create_roster.py')
+
+from baseball_app.models import Player
+
+Home = [[1, Scott, Podsednik], [2, Tadahito, Iguchi],[3, Carl, Everett], [4, Paul, Konerko], [5, Jermaine, Dye], [6, Aaron, Rowand], [7, AJ, Pierzynski], [8, Joe, Crede], [9, Juan, Uribe]]
+
+Away = [[1, Dexter, Fowler], [2, Kris, Bryant], [3, Anthony, Rizzo], [4, Ben, Zobrist], [5, Addison, Russell], [6, Jason, Heyward], [7, Jorge, Soler], [8, David, Ross], [9, Kyle, Schwarber]]
+
+for x in Home:
+    team = Home
+    order = x[0]
+    first_name = x[1]
+    last_name = x[2]
+    
+    Player.objects.create(team=team, order=order, first_name=first_name, last_name=last_name)
+
+for y in Away:
+    team = Away
+    order = y[0]
+    first_name = y[1]
+    last_name = y[2]
+    
+    Player.objects.create(team=team, order=order, first_name=first_name, last_name=last_name)
