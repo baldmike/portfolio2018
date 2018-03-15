7f57f8cb6483ad5051fd4b62320d289de1c73d28
Bald Mike <baldmike312@gmail.com>
2018-03-14 19:35:25 -0500

    Player Admin

--- apps/baseball_app/admin.py
+++ apps/baseball_app/admin.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from models import *
+
+class PlayerAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Player
+
+
+admin.site.register(Player, PlayerAdmin)
