f98adaf820e01e8771e0220997d0ecc31511581a
Bald Mike <baldmike312@gmail.com>
2018-03-14 19:12:44 -0500

    roster script

--- inits/create_roster.py
+++ inits/create_roster.py
@@ -7,7 +7,7 @@ from apps.baseball_app.models import Player
 
 Home = [[1, "Scott", "Podsednik"], [2, "Tadahito", "Iguchi"],[3, "Carl", "Everett"], [4, "Paul", "Konerko"], [5, "Jermaine", "Dye"], [6, "Aaron", "Rowand"], [7, "AJ", "Pierzynski"], [8, "Joe", "Crede"], [9, "Juan", "Uribe"]]
 
-Away = [[1, "Dexter", "Fowler"], [2, "Kris", "Bryant"], [3, "Anthony", "Rizzo"], [4, "Ben", "Zobrist"], [5, "Addison", "Russell"], [6, "Jason", "Heyward"], [7, "Jorge", "Soler"], [8, "David", "Ross"], [9, "Kyle", "Schwarber"]]
+Away = [[10, "Dexter", "Fowler"], [11, "Kris", "Bryant"], [12, "Anthony", "Rizzo"], [13, "Ben", "Zobrist"], [14, "Addison", "Russell"], [15, "Jason", "Heyward"], [16, "Jorge", "Soler"], [17, "David", "Ross"], [18, "Kyle", "Schwarber"]]
 
 for x in Home:
     team = Home
